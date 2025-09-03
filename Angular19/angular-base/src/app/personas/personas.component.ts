bccc7e8f6c7a2324d857098d13ed03f8ff44fb1f
Steven Villota <vfranklinestiven@gmail.com>
2025-09-03 16:14:58 -0500

    feat: add new angular project

---
+++ Angular19/angular-base/src/app/personas/personas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'personas-app',
+    templateUrl: './personas.component.html',
+    styleUrl: './personas.component.css'
+})
+
+export class micomponentepersona{
+    public mensaje= 'Mi mensaje';
+    public otromensaje= 'Segundo mensaje';
+}
