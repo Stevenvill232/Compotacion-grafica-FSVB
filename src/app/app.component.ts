ef5154b3fc9c452b847cc6b2ef7059a8a6dde938
Steven Villota <vfranklinestiven@gmail.com>
2025-11-28 21:57:11 -0500

    parte 2 del taller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Ejemplo1Component } from './ejemplo1/ejemplo1.component';
-import { micomponentepersona } from './personas/personas.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { ContentComponent } from './componentes/content/content.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ContenedorComponent } from './componentes/contenedor/contenedor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ejemplo1Component, micomponentepersona],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
+    ContenedorComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-base';
